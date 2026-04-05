e00e9480f22eca1bed71f050289e177a9f77fda2
ravi587 <ravi.pawar600@gmail.com>
2026-04-04 23:43:53 -0700

    Add test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+#print("Hello, World!")
+#a=10
+#b=30
+#print(a+b)'''
+
+a='hello ravi!'
+
+''' print(a.upper())
+print(a.lower())
+print(a.capitalize())
+print(a[1:5]) '''
+
+
+#print(a.replace('h','l'))
+print(a.split(' '))
+
+a='ravi'
+b="pawar"
+
+c=(a+b)
+print(c)
+c=(a+" "+b)
+print(c)
